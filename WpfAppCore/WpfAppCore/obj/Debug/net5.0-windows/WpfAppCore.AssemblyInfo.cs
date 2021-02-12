516fbb8acb7d6887dd3daa6775c0f513879ab394
neocyborg <ramt@outlook.com>
2021-02-12 13:22:20 +0530

    Visual studio 2019 wpf core app

--- WpfAppCore/WpfAppCore/obj/Debug/net5.0-windows/WpfAppCore.AssemblyInfo.cs
+++ WpfAppCore/WpfAppCore/obj/Debug/net5.0-windows/WpfAppCore.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("MyWPFApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WpfAppCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("MyWPFApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("MyWPFApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("WpfAppCore")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WpfAppCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
